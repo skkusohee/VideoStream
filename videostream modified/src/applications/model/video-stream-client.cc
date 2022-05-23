6ecedef1d607b4a7083247528e341c3245b07baa
budaPunk <85098491+budaPunk@users.noreply.github.com>
2022-05-24 00:02:26 +0900

    Update video-stream-client.cc

--- videostream modified/src/applications/model/video-stream-client.cc
+++ videostream modified/src/applications/model/video-stream-client.cc
@@ -48,7 +48,7 @@ namespace ns3
     m_frameSize = 0;
     m_resolution = 76800; // 글로벌 변수 RESOLUTION과 같은값 넣어주세요
     m_frameRate = 1;
-    m_videoSpeed = 1;   // 이거 골라서 넣으세용 디폴트 1배속
+    m_videoSpeed = 1;     // 이거 골라서 넣으세용 디폴트 1배속
     m_stopCounter = 0;
     m_lastRecvFrame = 0;
     m_rebufferCounter = 0;
@@ -167,7 +167,6 @@ namespace ns3
     uint32_t RES;
     uint32_t LRF;
     uint32_t FR;
-
     sscanf((char *)send_Buffer, "res:%u lrf:%u fr:%u", &RES, &LRF, &FR);
     printf("%d, %d, %d\n", RES, LRF, FR);
     //~TEST
@@ -193,55 +192,48 @@ namespace ns3
       NS_LOG_INFO("At time " << Simulator::Now().GetSeconds() << "s client sent 10 bytes to " << Inet6SocketAddress::ConvertFrom(m_peerAddress).GetIpv6() << " port " << Inet6SocketAddress::ConvertFrom(m_peerAddress).GetPort());
     }
   }
-
+  // 받아서 정리해놓은 프레임들을 소비하고 서버에 5초간의 영상을 요청하는 역할
   uint32_t VideoStreamClient::ReadFromBuffer(void) {
-    // 앞으로 5초어치의 프레임 요청
-    if(m_lastRecvFrame < MAX_PACKET_SIZE){
-      uint8_t send_Buffer[MAX_PACKET_SIZE];
-      sprintf((char *)send_Buffer, "res:%u lrf:%u fr:%u", m_resolution, m_lastRecvFrame, m_frameRate * m_videoSpeed);
-      Ptr<Packet> firstPacket = Create<Packet>(send_Buffer, MAX_PACKET_SIZE);
-      m_socket->Send(firstPacket);
-
-      m_bufferEvent = Simulator::Schedule(Seconds(1.0), &VideoStreamClient::ReadFromBuffer, this);
-      
-      return (m_currentBufferSize);
-    } else {
-      return(-1);
-    }
-
-    /*
-    // NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << " s, last buffer size: " << m_lastBufferSize << ", current buffer size: " << m_currentBufferSize);
-    if (m_currentBufferSize < m_frameRate) {
-      if (m_lastBufferSize == m_currentBufferSize) {
-        m_stopCounter++;
-        // If the counter reaches 3, which means the client has been waiting for 3 sec, and no packets arrived.
-        // In this case, we think the video streaming has finished, and there is no need to schedule the event.
-        if (m_stopCounter < 3) {
-          m_bufferEvent = Simulator::Schedule(Seconds(1.0), &VideoStreamClient::ReadFromBuffer, this);
-        }
-      } else {
-        NS_LOG_INFO("At time " << Simulator::Now().GetSeconds() << " s: Not enough frames in the buffer, rebuffering!");
-        m_stopCounter = 0; // reset the stopCounter
+    //************요약***************
+    // 원래는 여기서 프레임이 늘어나는지를 계산해서
+    // 늘어나지 않는 상태가 3초면 영상이 끝났다고 판별하는 부분이 있었고
+    // 늘어나지만 소비할 수 없을 정도로 늘어나면 버퍼링이 들어간다고 판별하는 부분이 있었습니다.
+    // 하지만 새로 짠 코드에서는 TOTAL_VIDEO_FRAME으로 영상의 총 길이를 저장하고 있기 때문에
+    // 이를통해 영상이 끝났는지를 판별할 수 있어서 3초동안 정지면 영상끝 부분 제거하였고
+    // 버퍼링 측정 코드는 사용하시려면 추가 하셔야 할 수 있습니다.
+    
+    // 소비할 수 없는만큼 버퍼에 영상의 프레임이 남아있는 경우
+    if(m_currentBufferSize < m_frameRate * m_videoSpeed){
+      // 마지막 수신한 프레임이 끝 프레임이 아니라면 즉 영상을 아직 받아야 하는 상태라면
+      if(m_lastRecvFrame < TOTAL_VIDEO_FRAME){
+        // 버퍼링 케이스에 해당합니다. 
         m_rebufferCounter++;
-        m_bufferEvent = Simulator::Schedule(Seconds(1.0), &VideoStreamClient::ReadFromBuffer, this);
+      } else{
+        // 영상이 모두 수신되었고 자투리 부분이 남아있던 것이라면 남은 영상 프레임을 클리어 해줍니다.
+        m_currentBufferSize = 0;
       }
-      m_lastBufferSize = m_currentBufferSize;
-      return (-1);
+      return(-1);
+    // 소비 가능한 만큼 영상이 남아있다면
     } else {
-      NS_LOG_INFO("At time " << Simulator::Now().GetSeconds() << " s: Play video frames from the buffer");
-      if (m_stopCounter > 0)
-        m_stopCounter = 0; // reset the stopCounter
-      if (m_rebufferCounter > 0)
-        m_rebufferCounter = 0; // reset the rebufferCounter
+      // 1초어치 영상을 소비합니다.
       m_currentBufferSize -= m_frameRate * m_videoSpeed;
-
+      // 영상이 최근 재생되었으므로 버퍼링 횟수도 초기화 합니다.
+      m_rebufferCounter = 0;
+      // 마지막 수신한 프레임이 끝 프레임이 아니라면 즉 영상을 아직 받아야 하는 상태라면
+      if(m_lastRecvFrame < TOTAL_VIDEO_FRAME){
+        // 패킷에 수신하고자 하는 화질, 몇번째 프레임부터 서버에서 송신해주면 되는지, 초당 몇개의 프레임을 소비하는지 의 데이터를 담아서 서버로 요청을 넣습니다.
+        // 서버쪽에서는 요청을 받으면 몇번째 프레임부터 client가 받고싶어하는지 에서부터 앞으로 5초간의 영상(초당 몇개 프레임 소비하는지 * 5) 만큼의 영상을 보내주게 됩니다.
+        uint8_t send_Buffer[MAX_PACKET_SIZE];
+        sprintf((char *)send_Buffer, "res:%u lrf:%u fr:%u", m_resolution, m_lastRecvFrame, m_frameRate * m_videoSpeed);
+        Ptr<Packet> firstPacket = Create<Packet>(send_Buffer, MAX_PACKET_SIZE);
+        m_socket->Send(firstPacket);
+      }
+      // 1초뒤에 다시 자신을 실행합니다.
       m_bufferEvent = Simulator::Schedule(Seconds(1.0), &VideoStreamClient::ReadFromBuffer, this);
-      m_lastBufferSize = m_currentBufferSize;
       return (m_currentBufferSize);
     }
-    */
   }
-
+  // 패킷을 통해 프레임 조각들을 받아서 프레임을 만드는 역할
   void VideoStreamClient::HandleRead(Ptr<Socket> socket) {
     NS_LOG_FUNCTION(this << socket);
 
@@ -251,29 +243,22 @@ namespace ns3
     while ((packet = socket->RecvFrom(from))) {
       socket->GetSockName(localAddress);
       if (InetSocketAddress::IsMatchingType(from)) {
+        // 서버에서 수신한 패킷에는 영상의 프레임 번호와 패킷 번호가 들어있습니다.
+        // 하나의 영상은 사이즈가 클 경우 여러 작은 패킷으로 쪼개져 보내지게 됩니다. 
         uint8_t recvData[packet->GetSize()];
         packet->CopyData(recvData, packet->GetSize());
         uint32_t frameNum;
-        uint32_t packetNum; 
+        uint32_t packetNum;
         sscanf((char *)recvData, "fn:%u pn:%u", &frameNum, &packetNum);
-        // 들어온거 체크
+        // 수신한 패킷에 대해서 수신 체크를 해줍니다. 
         m_FramePacketCounter[frameNum][packetNum] = true;
 
-        // printf("recv %d, %d\n", frameNum, packetNum);
-        // R 5
-        // M 2
-        // p_idx = {0, 1, 2}      {0, 1, 2}차있어야함
-        
-        // R 4
-        // M 2
-        // p_idx = {0, 1, 2}      {0, 1}차있어야함
-
-        // 마지막 프레임의 모든패킷이 다 들어왔는지 체크
         bool BREaK = false;
+        // 최근 들어온 프레임들에 대해서
         for (int frame_idx = m_lastRecvFrame; frame_idx < TOTAL_VIDEO_FRAME; frame_idx++) {
-          //printf("%d\n", m_lastRecvFrame);
+          // 해당 프레임들의 모든 패킷에 대해서 잘 수신되었는지 확인한다.
           for (int p_idx = 0; p_idx < RESOLUTION / MAX_PACKET_SIZE + 1; p_idx++) {
-            // false 있으면 바로나가
+            // false는 수신이 안된것을 표시하므로 바로 break합니다.
             if(m_FramePacketCounter[frame_idx][p_idx] == false) {
               BREaK = true;
               break;
@@ -283,13 +268,20 @@ namespace ns3
               if (RESOLUTION % MAX_PACKET_SIZE == 0){
                 // 프레임의 마지막 패킷이 도착한것이 확인되었을 경우 (R 4, M 2, p_idx = 1)
                 if (RESOLUTION / MAX_PACKET_SIZE == p_idx + 1){
+                  // 안정적으로 수신한 프레임을 수정합니다.
                   m_lastRecvFrame = frame_idx + 1;
+                  // 소비할 프레임 버퍼 사이즈도 증가시켜줍니다.
+                  m_currentBufferSize++;
                   break;
                 }
               // RESOLUTION 가 MAX_PACKET_SIZE로 안떨어질때
               } else {
+                // 프레임의 마지막 패킷이 도착한것이 확인되었을 경우
                 if (RESOLUTION / MAX_PACKET_SIZE == p_idx){
+                  // 안정적으로 수신한 프레임을 수정합니다.
                   m_lastRecvFrame = frame_idx + 1;
+                  // 소비할 프레임 버퍼 사이즈도 증가시켜줍니다.
+                  m_currentBufferSize++;
                   break;
                 }
               }
@@ -299,63 +291,8 @@ namespace ns3
             break;
           }
         }
-        // 프레임 사이즈 변경은 고민해보시고 작성해주세요!
-        // ** Your Code Here ** //
       }
     }
   }
 
 } // namespace ns3
-
-
-
-
-
-
-
-
-/*
-        if (frameNum == m_lastRecvFrame) {
-          m_frameSize += packet->GetSize();
-        } else {
-          if (frameNum > 0) {
-            NS_LOG_INFO("At time " << Simulator::Now().GetSeconds() << "s client received frame " << frameNum - 1 << " and " << m_frameSize << " bytes from " << InetSocketAddress::ConvertFrom(from).GetIpv4() << " port " << InetSocketAddress::ConvertFrom(from).GetPort());
-          }
-          m_currentBufferSize++;
-          m_lastRecvFrame = frameNum;
-          m_frameSize = packet->GetSize();
-        }
-
-        // The rebuffering event has happend 3+ times, which suggest the client to lower the video quality.
-        if (m_rebufferCounter >= 3) {
-          if (m_videoLevel > 1) {
-            NS_LOG_INFO("At time " << Simulator::Now().GetSeconds() << "s: Lower the video quality level!");
-            m_videoLevel--;
-            // reflect the change to the server
-            uint8_t dataBuffer[10];
-            sprintf((char *)dataBuffer, "%hu", m_videoLevel);
-            Ptr<Packet> levelPacket = Create<Packet>(dataBuffer, 10);
-            socket->SendTo(levelPacket, 0, from);
-            m_rebufferCounter = 0;
-          }
-        }
-
-        // If the current buffer size supports 5+ seconds video, we can try to increase the video quality level.
-        if (m_currentBufferSize > 5 * m_frameRate) {
-          if (m_videoLevel < MAX_VIDEO_LEVEL) {
-            m_videoLevel++;
-            // reflect the change to the server
-            uint8_t dataBuffer[10];
-            sprintf((char *)dataBuffer, "%hu", m_videoLevel);
-            Ptr<Packet> levelPacket = Create<Packet>(dataBuffer, 10);
-            socket->SendTo(levelPacket, 0, from);
-            m_currentBufferSize = m_frameRate;
-            NS_LOG_INFO("At time " << Simulator::Now().GetSeconds() << "s: Increase the video quality level to " << m_videoLevel);
-          }
-        }
-      }
-    }
-  }
-
-} // namespace ns3
-*/
